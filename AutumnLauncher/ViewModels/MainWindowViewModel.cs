d0924ef5e7fb1c3271eb391c25db1e9ccbcc60b8
hinuiiik <vknc@protonmail.com>
2023-07-13 12:45:22 -0400

    .

---
+++ AutumnLauncher/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AutumnLauncher.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ AutumnLauncher/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AutumnLauncher.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
